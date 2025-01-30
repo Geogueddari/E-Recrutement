eb21fea4a39bbe955ed5208fe42217df12505b4f
Geogueddari <abdeljabbarelgaddari24@gmail.com>
2025-01-30 17:46:36 +0100

    styling login page

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace E_Recrutement.Data
+{
+    public class ApplicationUser :IdentityUser
+    {
+        [Required]
+        public string UserName { get; set; }
+    }
+}
